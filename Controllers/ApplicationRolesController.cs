d9f61a8cae43575bf496eb5f9aa3b00d2b4df6fc
Nicholas Malmquist <nmmalmquist@crimson.ua.edu>
2022-10-18 16:57:19 -0500

    IMporvements!

--- Controllers/ApplicationRolesController.cs
+++ Controllers/ApplicationRolesController.cs
@@ -3,19 +3,22 @@ using crimson_closet.Data;
 using crimson_closet.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace crimson_closet.Controllers
 {
     public class ApplicationRolesController : Controller
     {
         private readonly RoleManager<ApplicationRole> _roleManager;
+        private readonly ApplicationDbContext _dbcontext;
 
 
 
-        public ApplicationRolesController(RoleManager<ApplicationRole> roleManager)
+        public ApplicationRolesController(RoleManager<ApplicationRole> roleManager, ApplicationDbContext dbcontext)
         {
             _roleManager = roleManager;
-
+            _dbcontext = dbcontext;
         }
 
         public IActionResult Index()
@@ -42,6 +45,64 @@ namespace crimson_closet.Controllers
 
             ModelState.AddModelError(string.Empty, "Role already exists.");
 
+            return View(role);
+        }
+
+        // GET: ApplicationRoles/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null || _roleManager.Roles == null)
+            {
+                return NotFound();
+            }
+
+            var role = await _roleManager.Roles
+                .FirstOrDefaultAsync(m => m.Id == id.ToString());
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            return View(role);
+        }
+
+        // POST: ApplicationRoles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            if (_roleManager.Roles == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Actor'  is null.");
+            }
+            var role = await _roleManager.FindByIdAsync(id.ToString());
+
+            if (role != null)
+            {
+                _dbcontext.Roles.Remove(role);
+                
+            }
+
+            await _dbcontext.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Actors/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null || _roleManager == null)
+            {
+                return NotFound();
+            }
+
+            var role = await _roleManager.Roles
+                .FirstOrDefaultAsync(m => m.Id == id.ToString());
+            if (role == null)
+            {
+                return NotFound();
+            }
+            //changed to throw the role with a list of movies
+            
             return View(role);
         }
 
--- Controllers/ApplicationUserRolesController.cs
+++ Controllers/ApplicationUserRolesController.cs
@@ -35,6 +35,7 @@ namespace crimson_closet.Controllers
 
         public IActionResult Create()
         {
+            //Adds to ViewBag all of the Users and Roles with Id as the identifier and the UserName/Name as the display
             ViewData["UserId"] = new SelectList(_dbcontext.Users, "Id", "UserName");
             ViewData["RoleId"] = new SelectList(_dbcontext.Roles, "Id", "Name");
 
@@ -46,17 +47,29 @@ namespace crimson_closet.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(ApplicationUserRole userRole)
         {
-            //var userRoleExists = _dbcontext.ApplicationUserRoles.Include(c => c.User)
-            //    .Include(c => c.Role).Any(o => (o.UserId.ToString() == userRole.User.Id && o.RoleId.ToString() == userRole.Role.Id));
-            Console.WriteLine(_dbcontext.ApplicationUserRoles);
-            if (false)
+            //Check to see if there is already a combo of User and Role like this already in database.
+            var search = _dbcontext.ApplicationUserRoles.Include(c => c.User).Include(c => c.Role)
+                .ToList().Where(c => (c.User.Id == userRole.UserId.ToString() && c.Role.Id == userRole.RoleId.ToString()));
+          
+            
+            if (search.Count() == 0)
             {
-                //role.name is used because Name is the property in IdentityRole that we need to set for the DB to update
+                //The userRole parameter comes into this method with only the Foreign Keys: UserID and RoleID
+                //We Then have to go get that specific user and role because we have to pass the role name and
+                //the full ApplicationUser object into the function "AddToRoleAsync"
+                //we use .ToList()[0]; in order to convert the query result into the class ApplicationRole and UserRole
+                userRole.User = _userManager.Users.Where(c => c.Id == userRole.UserId.ToString()).ToList()[0];
+                userRole.Role = _dbcontext.Roles.Where(c => c.Id == userRole.RoleId.ToString()).ToList()[0];
+
+                //Adds the role to the user
                 var result = await _userManager.AddToRoleAsync(userRole.User, userRole.Role.Name);
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction(nameof(Index)); //means success if we get to here
             }
 
             ModelState.AddModelError(string.Empty, "User Role combo already exists.");
+
+            ViewData["UserId"] = new SelectList(_dbcontext.Users, "Id", "UserName", userRole.UserId);
+            ViewData["RoleId"] = new SelectList(_dbcontext.Roles, "Id", "Name", userRole.RoleId);
 
             return View(userRole);
         }
