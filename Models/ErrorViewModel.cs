97cf2af4db80657030990f84188db08b85cdfe33
rakern <rakern@crimson.ua.edu>
2022-10-17 19:45:37 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace crimson_closet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
