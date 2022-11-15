58b59658c014b6a8291cfdc3a9547c68231bf0e6
rakern <rakern@crimson.ua.edu>
2022-11-15 13:08:22 -0600

    Edited closetController

--- Controllers/ClosetController.cs
+++ Controllers/ClosetController.cs
@@ -1,12 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using crimson_closet.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace crimson_closet.Controllers
 {
     public class ClosetController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _dbcontext;
+
+        public ClosetController(ApplicationDbContext context)
         {
-            return View();
+            _dbcontext = context;
+        }
+
+        public async Task<IActionResult> GetItemPhoto(Guid id)
+        {
+            var item = await _dbcontext.Item.FirstOrDefaultAsync(m => m.ItemId == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            var imageData = item.ItemPhoto;
+
+            return File(imageData, "image/jpg");
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _dbcontext.Item.Include(i => i.ItemType);
+            return View(await applicationDbContext.ToListAsync());
         }
     }
 }
